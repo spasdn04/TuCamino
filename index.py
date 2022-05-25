f8a86e9167e7d6cb118a749a994b8311ddd03fb7
spas <spas2004dn@gmail.com>
2022-05-25 19:40:00 +0200

    gran cambio en muchos archivos

--- index.py
+++ index.py
@@ -14,5 +14,9 @@ def estudios():
 def about():
     return render_template('about.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
